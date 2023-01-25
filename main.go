08d2cc1cdb42e0ee38fbfc700a9babb86e34a653
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-25 10:18:38 -0600

    Practicing with maps

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#FF0000",
+	// 	"green": "#00FF00",
+	// }
+
+	colors["white"] = "#FFFFFF"
+
+	delete(colors, "white")
+
+	fmt.Println(colors)
+}
